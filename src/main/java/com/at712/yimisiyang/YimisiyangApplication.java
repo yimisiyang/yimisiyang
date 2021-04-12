de28f3ba0add2af39df352c56d2e1576f74cb47b
yimisiyang <15230034878@163.com>
2021-04-12 18:45:19 +0800

    Initial commit

---
+++ src/main/java/com/at712/yimisiyang/YimisiyangApplication.java
@@ -0,0 +1,14 @@
+package com.at712.yimisiyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//程序的主入口
+@SpringBootApplication
+public class YimisiyangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YimisiyangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/at712/yimisiyang/YimisiyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.at712.yimisiyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YimisiyangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
