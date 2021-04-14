1b9c07c4a0fe7d508dbe64e81d92d4ecfec055de
yimisiyang <15230034878@163.com>
2021-04-14 20:58:38 +0800

    cxk:add Array.java and addTwoNumber function.

--- src/main/java/com/at712/yimisiyang/utils/Array.java
+++ src/main/java/com/at712/yimisiyang/utils/Array.java
@@ -1,5 +1,7 @@
 package com.at712.yimisiyang.utils;
 
+import com.at712.yimisiyang.utils.tools.ListNode;
+
 public class Array {
 
     /**
@@ -20,6 +22,39 @@ public class Array {
             }
         }
         return null;
+    }
+
+    /**
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null){
+            int n1 = l1 == null? 0:l1.val;
+            int n2 = l2 == null? 0:l2.val;
+            int sum = n1 + n2 + carry;
+            if(head == null){
+                head = tail = new ListNode(sum % 10);
+            }else{
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if(l1 != null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
+            }
+            if(carry > 0){
+                tail.next = new ListNode(carry);
+            }
+        }
+        return head;
     }
 
 
